901b88e6a1611973b49daef33b5f19a48b149de6
Vilmantas Petrusevičius <15175886+vilmantas@users.noreply.github.com>
2023-10-02 14:12:15 +0300

    Update menu scenes

--- Features/CapsuleBuildMenu/CapsuleTooltipController.cs
+++ Features/CapsuleBuildMenu/CapsuleTooltipController.cs
@@ -4,16 +4,21 @@ using System;
 public partial class CapsuleTooltipController : Control
 {
     [Export] public RichTextLabel Title;
-    [Export] public RichTextLabel Cost;
+    [Export] public RichTextLabel BuildCost;
+    [Export] public RichTextLabel BookingCost;
+    [Export] public RichTextLabel CostPerHour;
     
     public void Initialize(CapsuleConfiguration configuration)
     {
         Title.Text = configuration.Title;
-        Cost.Text = configuration.BuildCost.ToString("C");
+        BuildCost.Text = configuration.BuildCost.ToString("C");
+        BookingCost.Text = configuration.BookingCost.ToString("C");
+        CostPerHour.Text = configuration.CostPerHour.ToString("C");
     }
 
     public override void _Process(double delta)
     {
-        GlobalPosition = GetGlobalMousePosition();
+        Position = GetGlobalMousePosition() + new Vector2(10, 10);
+        GlobalPosition = Position;
     }
 }
